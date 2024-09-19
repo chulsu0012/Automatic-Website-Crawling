eaf9f25c59a3c3f1bf7ad295a9de492cb8574a96
[chulsu0012] <[chulsu0012@gmail.com]>
2024-09-19 21:02:36 +0900

    초기 커밋.

---
+++ Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace DataCrawling
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "yyyy/MM/dd";
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(59, 58);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(150, 28);
+            this.dateTimePicker1.TabIndex = 0;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.CustomFormat = "yyyy/MM/dd";
+            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker2.Location = new System.Drawing.Point(279, 58);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(150, 28);
+            this.dateTimePicker2.TabIndex = 1;
+            this.dateTimePicker2.ValueChanged += new System.EventHandler(this.dateTimePicker2_ValueChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(167, 114);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 70);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "실행";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(494, 231);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Name = "Form1";
+            this.Text = "Web Crawling Project";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+//Selenium Library
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+//EPPlus Library
+using OfficeOpenXml;
+
+namespace DataCrawling
+{
+    public partial class Form1 : Form
+    {
+        protected ChromeDriverService driverService = null;
+        protected ChromeOptions options = null;
+        protected ChromeDriver driver = null;
+
+        String url = "https://www.g2b.go.kr/index.jsp";
+        String keyword = "RPA";
+        String startDate;
+        String endDate;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            try
+            {
+                driverService = ChromeDriverService.CreateDefaultService();
+                driverService.HideCommandPromptWindow = true;
+
+                options = new ChromeOptions();
+                options.AddArgument("disable-gpu");
+            }
+            catch (Exception ex)
+            {
+                Trace.WriteLine(ex.Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dateTimePicker1.Value = DateTime.Today.AddDays(-4);
+            dateTimePicker2.Value = DateTime.Today;
+        }
+        
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            DateTime date = dateTimePicker1.Value;
+            startDate = date.ToString("yyyy'/'MM'/'dd");
+        }
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            DateTime date = dateTimePicker2.Value;
+            endDate = date.ToString("yyyy'/'MM'/'dd");
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (dateTimePicker1.Value > dateTimePicker2.Value)
+            {
+                MessageBox.Show("시작 날짜가 종료 날짜보다 큽니다. 다시 선택하세요.", "날짜 오류", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                Search();
+                List<List<String>> data = getData();
+                if (data.Count > 0) SaveExcel(data);
+                else MessageBox.Show("기간 내 데이터가 존재하지 않습니다.");
+                Application.Exit();
+            }
+        }
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void Search()
+        {
+            try
+            {
+                // Chrome으로 검색 진행
+                driver = new ChromeDriver(driverService, options);
+
+                // 링크의 웹 사이트로 이동
+                driver.Navigate().GoToUrl(url);
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+                // 공고명
+                var searchBox = driver.FindElement(By.XPath("//*[@id='bidNm']"));
+                searchBox.SendKeys(keyword);
+
+                // 공고일자
+                var startInput = driver.FindElement(By.XPath("//*[@id='fromBidDt']"));
+                startInput.Clear();
+                startInput.SendKeys(startDate);
+
+                var endInput = driver.FindElement(By.XPath("//*[@id='toBidDt']"));
+                endInput.Clear();
+                endInput.SendKeys(endDate);
+
+                // 검색
+                var searchButton = driver.FindElement(By.XPath("//*[@id='searchForm']/div/fieldset[1]/ul/li[4]/dl/dd[3]/a/strong"));
+                searchButton.Click();
+            }
+            catch (Exception exc)
+            {
+                Trace.WriteLine(exc.Message);
+            }
+        }
+
+        private List<List<String>> getData()
+        {
+            List<List<String>> resultData = new List<List<String>>();
+
+            driver.SwitchTo().Frame("sub");
+            driver.SwitchTo().Frame("main");
+
+            var table = driver.FindElement(By.XPath("//*[@id='resultForm']/div[2]/table"));
+            var tbody = table.FindElement(By.TagName("tbody"));
+            var trs = tbody.FindElements(By.TagName("tr"));
+
+            foreach (var tr in trs)
+            {
+                List<String> data = new List<String>();
+                var tds = tr.FindElements(By.TagName("td"));
+
+                if (tds.Count() <= 1)
+                {
+                    break;
+                }
+
+                data.Add(tds[3].Text);
+                data.Add(keyword);
+                for (int i = 4; i < 6; i++)
+                {
+                    data.Add(tds[i].Text);
+                }
+                data.Add(Regex.Replace(tds[7].Text, @"\s*\(.*?\)\s*", String.Empty).Replace('/', '-'));
+                data.Add(DateTime.Now.ToString("yyyy-MM-dd HH:mm"));
+
+                resultData.Add(data);
+            }
+
+            driver.Quit();
+            return resultData;
+        }
+
+        private void SaveExcel(List<List<String>> data)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                openFileDialog.Filter = "Excel files (*.xlsx)|*.xlsx";
+                openFileDialog.FilterIndex = 1;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    FileInfo fileInfo = new FileInfo(openFileDialog.FileName);
+
+                    using (ExcelPackage package = new ExcelPackage(fileInfo))
+                    {
+                        ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
+
+                        int startRow = worksheet.Dimension.End.Row + 1;
+
+                        for (int i = 0; i < data.Count; i++)
+                        {
+                            for (int j = 0; j < data[i].Count; j++)
+                            {
+                                worksheet.Cells[startRow + i, j + 1].Value = data[i][j];
+                            }
+                        }
+
+                        package.Save();
+                    }
+                }
+            }
+        }
+    }
+}
